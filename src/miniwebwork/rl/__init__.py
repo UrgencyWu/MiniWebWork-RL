e66fc260f08b46dedeeec767b74710d25658cdbf
Shaohua Wu <109404649+UrgencyWu@users.noreply.github.com>
2026-07-31 15:39:09 +0800

    refactor: expose streaming trajectory loss

--- src/miniwebwork/rl/__init__.py
+++ src/miniwebwork/rl/__init__.py
@@ -14,14 +14,17 @@ from .objective import (
     clipped_trajectory_policy_loss,
     group_relative_advantages,
 )
+from .streaming import StreamingTrajectoryLoss, clipped_single_trajectory_loss
 
 __all__ = [
     "NoRewardVarianceError",
     "PolicyLossResult",
     "ReplayGroup",
+    "StreamingTrajectoryLoss",
     "TrajectoryReplay",
     "TurnReplay",
     "build_replay_group",
+    "clipped_single_trajectory_loss",
     "clipped_trajectory_policy_loss",
     "group_relative_advantages",
     "old_policy_batch",
